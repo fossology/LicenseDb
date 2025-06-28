08389406e733a2f0e702537cdbc0d61279e8ee5e
chayandass <daschayan8837@gmail.com>
2025-06-28 23:30:58 +0530

    down migration pg_trgm index

--- pkg/db/migrations/000009_pg_trgm_and_index.down.sql
+++ pkg/db/migrations/000009_pg_trgm_and_index.down.sql
@@ -1,8 +1,10 @@
 -- SPDX-FileCopyrightText: 2025 Chayan Das <01chayandas@gmail.com>
 -- SPDX-License-Identifier: GPL-2.0-only
 
--- Drop the GIN index
+-- Drop the GIN indexs
 DROP INDEX IF EXISTS trgm_rf_text_idx;
+DROP INDEX IF EXISTS trgm_text_idx;
+
 
 -- Drop pg_trgm extension
 DROP EXTENSION IF EXISTS pg_trgm;
