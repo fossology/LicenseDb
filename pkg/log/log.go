da8f13fccf85beb21b9263356760ed0a8c273703
chayandass <daschayan8837@gmail.com>
2025-07-05 16:02:08 +0530

    feat(refresh_token): add refresh token funtionality and zap logger

---
+++ pkg/log/log.go
@@ -0,0 +1,34 @@
+package log
+
+import "go.uber.org/zap"
+
+var logger *zap.Logger
+
+func LogInfo(msg string, fields ...zap.Field) {
+	logger.Info(msg, fields...)
+
+}
+
+func LogError(msg string, fields ...zap.Field) {
+	logger.Error(msg, fields...)
+}
+
+func LogWarn(msg string, fields ...zap.Field) {
+	logger.Warn(msg, fields...)
+}
+
+func LogFatal(msg string, fields ...zap.Field) {
+	logger.Fatal(msg, fields...)
+}
+
+func LogDebug(msg string, fields ...zap.Field) {
+	logger.Debug(msg, fields...)
+}
+func init() {
+
+	cfg := zap.NewProductionConfig()
+	cfg.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+	cfg.DisableStacktrace = true
+	baseLogger, _ := cfg.Build()
+	logger = baseLogger.WithOptions(zap.AddCallerSkip(1))
+}
