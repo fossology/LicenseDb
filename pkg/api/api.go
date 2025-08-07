8128dffa493b29169ca268c45c8d98d55ec0b742
Chayan Das <01chayandas@gmail.com>
2025-08-07 20:09:54 +0530

    feat :add refresh token funtionality
    Signed-off-by: Chayan Das <01chayandas@gmail.com>

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -94,6 +94,10 @@ func Router() *gin.Engine {
 			{
 				login.POST("", auth.Login)
 			}
+			ref := unAuthorizedv1.Group("/refresh-token")
+			{
+				ref.POST("", auth.VerifyRefreshToken)
+			}
 			apiCollection := unAuthorizedv1.Group("/apiCollection")
 			{
 				apiCollection.GET("", GetAPICollection)
